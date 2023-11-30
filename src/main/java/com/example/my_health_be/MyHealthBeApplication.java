6f4cb472e22455f9a36aaca60bfbd35a216a3549
dongyun <0902hdy@gmail.com>
2023-11-30 21:54:20 +0900

    first commit

---
+++ src/main/java/com/example/my_health_be/MyHealthBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_health_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHealthBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHealthBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_health_be/MyHealthBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_health_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHealthBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
